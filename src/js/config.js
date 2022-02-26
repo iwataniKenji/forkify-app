02a159e752f60e0e0a7c7cfc8a339ec8f62f2d51
Kleverson Kenji <kleversonkenji@gmail.com>
2022-02-26 16:18:07 -0300

    adding helpers.js and config.js

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+// variáveis que serão reutilizadas
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -7,27 +9,22 @@ export const state = {
 // não retorna nada, mas muda objeto state acima
 export const loadRecipe = async function (id) {
   try {
+    const data = await getJSON(`${API_URL}/${id}`)
 
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      );
-      const data = await res.json();
-      
-      if (!res.ok) throw new Error(`${data.message} (${res.status})`); // pega a mensagem de erro do objeto
-      
-      const { recipe } = data.data;
-      state.recipe = {
-        id: recipe.id,
-        title: recipe.title,
-        publisher: recipe.publisher,
-        sourceUrl: recipe.source_url,
-        image: recipe.image_url,
-        servings: recipe.servings,
-        cookingTime: recipe.cooking_time,
-        ingredients: recipe.ingredients,
-      };
-      console.log(state.recipe);
-    } catch (err) {
-      alert(err);
-    }
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    // temp
+    console.error(`${err}`);
+  }
 };
